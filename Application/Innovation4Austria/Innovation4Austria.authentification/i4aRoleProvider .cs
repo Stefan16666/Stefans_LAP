42c6c7a335a3971f676d41da1e4d8847f7a2fa70
groistef <groistef@PC901B0E043B02>
2017-01-31 13:20:08 +0100

    Authentification impementiert

--- Application/Innovation4Austria/Innovation4Austria.authentification/i4aRoleProvider .cs
+++ Application/Innovation4Austria/Innovation4Austria.authentification/i4aRoleProvider .cs
@@ -1,4 +1,5 @@
-﻿using Innovation4Austria.logic;
+﻿using innovation4austria.data;
+using innovation4austria.logic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,12 +7,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Security;
 
-namespace Innovation4Austria.authentification
+namespace innovation4austria.authentication
 {
-    public class i4aRoleProvider : RoleProvider
+    class i4aRoleProvider : RoleProvider
     {
-
-        string applicationName = "Innovation4Austria database";
+        string applicationName = "innovation4austria database";
         public override string ApplicationName
         {
             get
@@ -22,10 +22,99 @@ namespace Innovation4Austria.authentification
             set
             {
                 if (!string.IsNullOrEmpty(value))
-                {
                     applicationName = value;
-                }
             }
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            List<User> roleUsers = RoleAdministration.GetRoleUsers(roleName);
+            if (roleUsers != null)
+            {
+                return roleUsers.Where(x => x.Username.Contains(usernameToMatch)).Select(x => x.Username).ToArray();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public override string[] GetAllRoles()
+        {
+            List<Role> allRoles = RoleAdministration.GetRoles();
+
+            if (allRoles != null)
+            {
+                return allRoles.Select(x => x.Name).ToArray();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            Role userRoles = RoleAdministration.GetUserRole(username);
+
+            if (userRoles != null)
+            {
+                return new string[] { userRoles.Name };
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+
+            List<User> roleUsers = RoleAdministration.GetRoleUsers(roleName);
+            if (roleUsers != null)
+            {
+                return roleUsers.Select(x => x.Username).ToArray();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            string[] userRoles = GetRolesForUser(username);
+            if (userRoles != null)
+            {
+                return userRoles.Contains(roleName);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            string[] allRoles = GetAllRoles();
+            if (allRoles != null)
+            {
+                return allRoles.Contains(roleName);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        #region NotImplementedMember
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
         }
         public override void AddUsersToRoles(string[] usernames, string[] roleNames)
         {
@@ -36,81 +125,6 @@ namespace Innovation4Austria.authentification
         {
             throw new NotImplementedException();
         }
-
-        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        ///  searching for all names of a role
-        /// </summary>
-        /// <param name="roleName"></param>
-        /// <param name="usernameToMatch"></param>
-        /// <returns></returns>
-        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// return all roles
-        /// </summary>
-        /// <returns></returns>
-        public override string[] GetAllRoles()
-        {
-            List<Rolle> allRolles = RoleAdministration.GetRoles();
-            if (allRolles != null)
-            {
-                return allRolles.Select(x => x.Bezeichnung).ToArray();
-            }
-            else
-            {
-                return null;
-            }
-        }
-        
-        /// <summary>
-        ///  get all roles which the user has
-        /// </summary>
-        /// <param name="username"></param>
-        /// <returns></returns>
-        public override string[] GetRolesForUser(string username)
-        {
-            Rolle userRoles = RoleAdministration.GetUserRole(username);
-            if (userRoles != null)
-            {
-                return new string[] { userRoles.Bezeichnung };
-            }
-            else
-            {
-                return null;
-            }
-        }
-        /// <summary>
-        /// returns all user of a role
-        /// </summary>
-        /// <param name="roleName"></param>
-        /// <returns></returns>
-        public override string[] GetUsersInRole(string roleName)
-        {
-            List<Benutzer> userOfARole = RoleAdministration.GetRoleUser(roleName)
-            return null;
-        }
-
-        public override bool IsUserInRole(string username, string roleName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override bool RoleExists(string roleName)
-        {
-            throw new NotImplementedException();
-        }
+        #endregion
     }
 }
