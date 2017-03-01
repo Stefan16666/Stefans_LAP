6ac2266840452c0d76c679a1b5c70be22654a83d
groistef <groistef@PC901B0E043B02>
2017-03-01 10:15:29 +0100

    datetimerange nuGet heruntergeladen

--- Application/Innovation4Austria/Innovation4Austria.web/App_Start/BundleConfig.cs
+++ Application/Innovation4Austria/Innovation4Austria.web/App_Start/BundleConfig.cs
@@ -8,9 +8,7 @@ namespace Innovation4Austria.web
         // Weitere Informationen zu Bundling finden Sie unter "http://go.microsoft.com/fwlink/?LinkId=301862"
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.js",
-            //            "~/Scripts/jquery.easing.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                       "~/Scripts/jquery-{version}.js"));
 
@@ -19,6 +17,12 @@ namespace Innovation4Austria.web
 
             bundles.Add(new ScriptBundle("~/bundles/toastr").Include(
                         "~/Scripts/toastr.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery.daterangepicker").Include(
+                "~/Scripts/jquery.daterangepicker/date-en-US.js",
+                "~/Scripts/jquery.daterangepicker/date.js",
+                "~/Scripts/jquery.daterangepicker/daterangepicker.jQuery.js",
+                "~/Scripts/jquery.daterangepicker/time.js"));
 
             // Verwenden Sie die Entwicklungsversion von Modernizr zum Entwickeln und Erweitern Ihrer Kenntnisse. Wenn Sie dann
             // für die Produktion bereit sind, verwenden Sie das Buildtool unter "http://modernizr.com", um nur die benötigten Tests auszuwählen.
@@ -31,7 +35,10 @@ namespace Innovation4Austria.web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
-                "~/Content/style.css"));
+                "~/Content/style.css",
+                "~/Content/daterangepicker.css",
+                "~/Content/jquery.daterangepicker/ui.daterangepicker.css"
+                 ));
 
             bundles.Add(new StyleBundle("~/Content/toastr").Include(
                 "~/Content/toastr.css"));
